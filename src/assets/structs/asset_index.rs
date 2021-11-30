ebcae8c1a80cc405feef1e69d6568bae3e49996c
suyashtnt <suyashtnt@gmail.com>
2021-11-30 15:15:07 +0200

    feat(assets): :construction: Add stub function for saving assets

--- src/assets/structs/asset_index.rs
+++ src/assets/structs/asset_index.rs
@@ -1,10 +1,16 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error};
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AssetIndex {
     pub objects: HashMap<String, Object>,
+}
+
+impl AssetIndex {
+    pub async fn save_assets(save_path: &str) -> Result<(), Box<dyn Error>> {
+        todo!()
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
