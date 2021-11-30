e06eb7ec9d02a20ddb72502d2f4038ca6bdb56ff
suyashtnt <suyashtnt@gmail.com>
2021-11-30 15:55:49 +0200

    feat(assets): :construction: more work on saving assets

--- src/assets/structs/asset_index.rs
+++ src/assets/structs/asset_index.rs
@@ -8,8 +8,31 @@ pub struct AssetIndex {
 }
 
 impl AssetIndex {
-    pub async fn save_assets(save_path: &str) -> Result<(), Box<dyn Error>> {
-        todo!()
+    pub async fn save_assets(&self, save_path: &str) -> Result<(), Box<dyn Error>> {
+        let mut tasks = Vec::new();
+        let path_and_url: HashMap<String, String> = self
+            .objects
+            .iter()
+            .map(|(path, object)| {
+                let url = format!(
+                    "http://resources.download.minecraft.net/{}/{}/",
+                    &object.hash[..2],
+                    object.hash
+                );
+
+                (path.clone(), url)
+            })
+            .collect();
+
+        for (path, url) in path_and_url.iter() {
+            tasks.push(tokio::spawn(async move { todo!() }))
+        }
+
+        for task_handle in tasks {
+            task_handle.await.unwrap()
+        }
+
+        Ok(())
     }
 }
 
@@ -17,4 +40,32 @@ impl AssetIndex {
 pub struct Object {
     pub hash: String,
     pub size: i64,
+}
+
+mod tests {
+    #[tokio::test]
+    async fn test_saving() {
+        use crate::assets::structs::launcher_meta::LauncherMeta;
+        let server_url = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+
+        let response = reqwest::get(server_url)
+            .await
+            .unwrap()
+            .json::<LauncherMeta>()
+            .await
+            .unwrap();
+
+        response
+            .latest
+            .version_for_release(&response)
+            .version_manifest()
+            .await
+            .unwrap()
+            .asset_index()
+            .await
+            .unwrap()
+            .save_assets("tests-dir")
+            .await
+            .unwrap();
+    }
 }
