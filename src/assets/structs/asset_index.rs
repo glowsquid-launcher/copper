92cfcf2c9085d5d8f0b81e2bdd18b87e4f633da9
suyashtnt <suyashtnt@gmail.com>
2021-11-30 17:02:44 +0200

    feat(assets): :sparkles: Add the ability to save assets

--- src/assets/structs/asset_index.rs
+++ src/assets/structs/asset_index.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error};
+use std::{collections::HashMap, error::Error, fs::create_dir_all, io::Write};
 
 use serde::{Deserialize, Serialize};
 
@@ -8,24 +8,36 @@ pub struct AssetIndex {
 }
 
 impl AssetIndex {
-    pub async fn save_assets(&self, save_path: &str) -> Result<(), Box<dyn Error>> {
+    /// The save path should be /assets/objects
+    pub async fn save_assets(&self, save_path: String) -> Result<(), Box<dyn Error>> {
         let mut tasks = Vec::new();
         let path_and_url: HashMap<String, String> = self
             .objects
             .iter()
-            .map(|(path, object)| {
+            .map(|(_path, object)| {
                 let url = format!(
-                    "http://resources.download.minecraft.net/{}/{}/",
+                    "http://resources.download.minecraft.net/{}/{}",
                     &object.hash[..2],
                     object.hash
                 );
 
-                (path.clone(), url)
+                (format!("{}/{}", &object.hash[..2], object.hash), url)
             })
             .collect();
 
-        for (path, url) in path_and_url.iter() {
-            tasks.push(tokio::spawn(async move { todo!() }))
+        for (path, url) in path_and_url.into_iter() {
+            let save_path_clone = save_path.clone();
+            let mut path_without_last_vec = path.split("/").collect::<Vec<&str>>();
+            path_without_last_vec.pop();
+            let path_without_last = path_without_last_vec.join("/");
+
+            tasks.push(tokio::spawn(async move {
+                let mut response = reqwest::get(url).await.unwrap().bytes().await.unwrap();
+                create_dir_all(format!("{}/{}", &save_path_clone, &path_without_last)).unwrap();
+                let mut file =
+                    std::fs::File::create(format!("{}/{}", save_path_clone, &path)).unwrap();
+                file.write(&mut response).unwrap();
+            }))
         }
 
         for task_handle in tasks {
@@ -48,6 +60,14 @@ mod tests {
         use crate::assets::structs::launcher_meta::LauncherMeta;
         let server_url = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
 
+        println!(
+            "{}",
+            std::env::current_dir()
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+        );
+
         let response = reqwest::get(server_url)
             .await
             .unwrap()
@@ -64,7 +84,13 @@ mod tests {
             .asset_index()
             .await
             .unwrap()
-            .save_assets("tests-dir")
+            .save_assets(
+                std::env::current_dir()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string()
+                    + "/tests-dir",
+            )
             .await
             .unwrap();
     }
