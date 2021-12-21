d5f7be45a297407b42944fa7d2b0d0873d999c1d
suyashtnt <suyashtnt@gmail.com>
2021-12-21 23:27:47 +0200

    feat(standalone): :sparkles: add decent logging

--- minecraft-cli/src/cli.rs
+++ minecraft-cli/src/cli.rs
@@ -1,16 +1,22 @@
-use std::{error::Error, path::PathBuf};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use log::info;
+use std::path::PathBuf;
 use structopt::StructOpt;
+use tokio::task::JoinHandle;
 
+use anyhow::{anyhow, Result};
 use minecraft_rs::assets::structs::launcher_meta::LauncherMeta;
 use minecraft_rs::util::DivPathBuf;
 
-pub async fn handle_args(args: Args) -> Result<(), Box<dyn Error>> {
+pub async fn handle_args(args: Args) -> Result<()> {
     match args {
         Args::DownloadDependencies {
             root,
             version: version_id,
         } => {
-            let launcher_meta = LauncherMeta::download_meta().await?;
+            let launcher_meta = LauncherMeta::download_meta()
+                .await
+                .map_err(|_e| anyhow!("Failed to download launcher meta"))?;
 
             let version_info = if version_id == "latest" {
                 launcher_meta.latest.version_for_release(&launcher_meta)
@@ -19,58 +25,120 @@ pub async fn handle_args(args: Args) -> Result<(), Box<dyn Error>> {
                     .versions
                     .iter()
                     .find(|version| version.id == version_id)
-                    .unwrap()
+                    .ok_or(anyhow!("Version {} not found", version_id))?
             };
 
-            let version = version_info.version_manifest().await?;
+            let version = version_info.version_manifest().await.map_err(|_e| {
+                anyhow!(
+                    "Failed to download version manifest for version {}",
+                    &version_info.id
+                )
+            })?;
+
+            info!(
+                "Downloaded version manifest for version {}",
+                &version_info.id
+            );
 
             let root_path = DivPathBuf(PathBuf::from(root));
             let libraries_path = &root_path / "libraries";
             let version_path = &root_path / "versions" / &*version.id;
 
+            let bars = MultiProgress::new();
+            let style = ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7} {msg}");
+
+            let libraries_bar = bars.add(ProgressBar::new(1000));
+            let assets_bar = bars.add(ProgressBar::new(1000));
+
+            libraries_bar.set_style(style.clone());
+            assets_bar.set_style(style.clone());
+
+            libraries_bar.set_message("downloading libraries");
+            assets_bar.set_message("downloading assets");
+
             let mut libraries_watcher = version
                 .start_download_libraries(libraries_path.to_path_buf())
                 .await;
 
-            while let Ok(_) = libraries_watcher.progress_watcher.changed().await {
-                let progress = *libraries_watcher.progress_watcher.borrow();
-                println!(
-                    "libraries downloaded: {}/{}",
-                    progress.finished, progress.total_size
-                ); //derive copy on the DownloadProgress
-            }
-
-            libraries_watcher.download_task.await?;
-
             let mut asset_watcher = version
                 .asset_index()
-                .await?
+                .await
+                .map_err(|_e| {
+                    anyhow!(
+                        "Failed to download asset index for version {}",
+                        &version_info.id
+                    )
+                })?
                 .start_download_assets((&root_path / "assets" / "objects").to_path_buf())
                 .await;
 
-            while let Ok(_) = asset_watcher.progress_watcher.changed().await {
-                let progress = *asset_watcher.progress_watcher.borrow();
-                println!(
-                    "assets downloaded: {}/{}",
-                    progress.finished, progress.total_size
-                ); //derive copy on the DownloadProgress
-            }
+            libraries_bar.enable_steady_tick(100);
+            assets_bar.enable_steady_tick(100);
 
-            asset_watcher.download_task.await?;
+            let libraries: JoinHandle<Result<(), ()>> = tokio::spawn(async move {
+                while let Ok(_) = libraries_watcher.progress_watcher.changed().await {
+                    let progress = *libraries_watcher.progress_watcher.borrow();
+                    libraries_bar.clone().set_length(progress.total_size);
+                    libraries_bar.clone().set_position(progress.finished);
+                }
 
-            version.save_manifest_json(
-                (&version_path / &*format!("{}.json", &*version.id)).to_path_buf(),
-            )?;
+                libraries_watcher.download_task.await.map_err(|_err| ())?;
+                libraries_bar
+                    .clone()
+                    .finish_with_message("done downloading libraries");
 
-            println!("saved manifest");
+                Ok(())
+            });
+
+            let assets: JoinHandle<Result<(), ()>> = tokio::spawn(async move {
+                while let Ok(_) = asset_watcher.progress_watcher.changed().await {
+                    let progress = *asset_watcher.progress_watcher.borrow();
+                    assets_bar.clone().set_length(progress.total_size);
+                    assets_bar.clone().set_position(progress.finished);
+                }
+
+                asset_watcher.download_task.await.map_err(|_err| ())?;
+                assets_bar
+                    .clone()
+                    .finish_with_message("done downloading assets");
+
+                Ok(())
+            });
+
+            bars.join()?;
+
+            libraries
+                .await?
+                .map_err(|_e| anyhow!("Failed to download libraries"))?;
+
+            assets
+                .await?
+                .map_err(|_err| anyhow!("Failed to download assets"))?;
+
+            version
+                .save_manifest_json(
+                    (&version_path / &*format!("{}.json", &*version.id)).to_path_buf(),
+                )
+                .map_err(|_e| {
+                    anyhow!(
+                        "Failed to save version manifest for version {}",
+                        &version.id
+                    )
+                })?;
+
+            info!("saved manifest");
 
             version
                 .download_client_jar(
                     (&version_path / &*format!("{}.jar", &*version.id)).to_path_buf(),
                 )
-                .await?;
+                .await
+                .map_err(|_e| {
+                    anyhow!("Failed to download client jar for version {}", &version.id)
+                })?;
 
-            println!("saved client jar");
+            info!("saved client jar");
         }
     }
     Ok(())
--- minecraft-cli/src/main.rs
+++ minecraft-cli/src/main.rs
@@ -1,12 +1,36 @@
 mod cli;
 
+use anyhow::Result;
 use cli::{handle_args, Args};
-use std::error::Error;
+use fern::colors::{Color, ColoredLevelConfig};
 use structopt::StructOpt;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
+    setup_logger()?;
     let args = Args::from_args();
 
     handle_args(args).await
+}
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            let colours = ColoredLevelConfig::new()
+                .info(Color::Green)
+                .warn(Color::Yellow)
+                .error(Color::Red);
+
+            out.finish(format_args!(
+                "{} [{}] {}",
+                chrono::Local::now().format("[%H:%M:%S]"),
+                colours.color(record.level()),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
 }
